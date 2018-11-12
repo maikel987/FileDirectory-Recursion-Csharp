c8b616f77ccb98dd9b5a0e6672e3db3564af64c9
Maikel SITBON <maikelsitbon@gmail.com>
2018-11-12 15:00:06 +0200

    Add files via upload

---
+++ FileDirectory Recursion Csharpe/FileDirectory/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo directo = new DirectoryInfo(@"C:\Users\Owner\Documents\Visual Studio 2015\Projects");
+            FileDirectory(directo);
+        }
+
+        static void FileDirectory(DirectoryInfo directory)
+        {
+            DirectoryInfo[] subDirs = directory.GetDirectories();
+            for (int i = 0; i < subDirs.Length; i++)
+            {
+                Console.WriteLine("/dir - {0}", subDirs[i].Name);
+                FileDirectory(subDirs[i]);
+
+            }
+            FileInfo[] subFiles = directory.GetFiles();
+            for (int i = 0; i < subFiles.Length; i++)
+            {
+                Console.WriteLine("\t file - {0}", subFiles[i].Name);
+
+            }
+         
+        }
+    }
+    }
+
